1dcac3f9f362cea482dd0046cdef9d95ebce1b5b
Ash Winter <ash_winter@hotmail.co.uk>
2017-01-22 18:52:15 +0000

    Added a form to the CfP pages

--- routes/callforpapers.js
+++ routes/callforpapers.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('callforpapers', { title: 'Leeds Testing Atelier - Call for Papers' });
 });
 
+/* Post a new idea */
+router.post('/', function(req, res, next) {
+    console.log(req.body.email);
+    res.send('Post page');
+});
+
 module.exports = router;
