5a03df5abee57e3208ccef450e444d283755bd1c
Ash Winter <ash_winter@hotmail.co.uk>
2017-02-13 23:18:42 +0000

    Added basic iteration through speakers data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var speakerData = require('../data/speaker_details.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Leeds Testing Atelier' });
-});
+    res.render('index', speakerData);
+ });
 
 module.exports = router;
