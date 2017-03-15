606f50ef316246997efc9b6cf4cd588c3d46f6de
Ash Winter <ash_winter@hotmail.co.uk>
2017-03-15 16:58:30 +0000

    Scaffold for a sponsors page

---
+++ routes/sponsors.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var sponsorData = require('../data/sponsor_details');
+
+
+/* GET sponsors page. */
+router.get('/', function(req, res, next) {
+    res.render('sponsors', sponsorData);
+});
+
+module.exports = router;
