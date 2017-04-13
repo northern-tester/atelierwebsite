ca6e3fbfc18ecdcda899f14da2c03faaa554b2af
Ash Winter <ash_winter@hotmail.co.uk>
2017-04-13 08:58:20 +0100

    Added slack config, as in did it proper like

--- routes/callforpapers.js
+++ routes/callforpapers.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
-var emailConfig = require('../config/email_example.json');
+var slackConfig = require('../config/slack.json');
 var Slack = require('slack-node');
 
 /* GET CfP page. */
@@ -9,26 +8,17 @@ router.get('/', function(req, res, next) {
   res.render('callforpapers', { title: 'Testing Atelier - Call for Papers' });
 });
 
-/* Create a Node Mailer Transporter Object */
-var transporter = nodemailer.createTransport({
-        service: emailConfig.service,
-        auth: {
-            user: emailConfig.username,
-            pass: emailConfig.password
-        }
-    });
-
 /* Post a new idea via Slack */
 router.post('/', function(req, res, next) {
 			//Config
-			webhookUri = "https://hooks.slack.com/services/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";
+			webhookUri = slackConfig.webhookuri;
 			//New slack object
 			slack = new Slack();
 			slack.setWebhook(webhookUri);
 			//Build up the message and post
 			slack.webhook({
-		  		channel: "#callforpapers",
-		 		username: "Call for Papers",
+		  		channel: slackConfig.channel,
+		 		username: slackConfig.username,
 		  		text: "Name: "+req.body.name+"\n\nEmail: "+req.body.email+"\n\nBio: "+req.body.bio+"\n\nTitle: "+req.body.title+"\n\nAbstract: "+req.body.abstract+"\n\nTwitter: "+req.body.twitter
 			}, function(err, response) {
 				if(err) {
