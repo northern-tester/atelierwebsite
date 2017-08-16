71314cf55d4add284dc56feeb3f0d1ae665562a7
Ash Winter <ash_winter@hotmail.co.uk>
2017-08-16 17:43:22 +0100

    Also added a callback method for when the user has ticked the recaptcha and forgotten to submit and timeout has occurred

--- public/javascript/atelier.js
+++ public/javascript/atelier.js
@@ -1,3 +1,7 @@
     function recaptcha_callback() {
        document.getElementById('cfpSubmit').disabled = false;
     }
+
+    function recaptcha_expired_callback() {
+       document.getElementById('cfpSubmit').disabled = true;
+    }
