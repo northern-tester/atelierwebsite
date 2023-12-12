7033bffc00f13ac0e8db64fc74e896108accb968
Ash Winter <ash_winter@hotmail.co.uk>
2023-12-12 21:35:17 +0000

    Missing comma problems

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/sponsors', sponsors);
 app.use('/callforpapers', callforpapers);
 app.use('/previousateliers', previousateliers);
 app.use('/thankyou', thankyou);
-app.use('/sponsors', sponsors);
 app.use('/robot', robot);
 
 // catch 404 and forward to error handler
@@ -70,12 +70,12 @@ app.use(function(_req, _res, next, err) {
 // error handler
 app.use(function(err, req, res, _next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.error;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render(err.message);
+  res.render(err.error);
 });
 
 module.exports = app;
